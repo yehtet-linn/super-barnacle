e986677e0467c3339e7ba886d8a837e25d060f9a
Ye Htet Lin <yehtet.linyn@gmail.com>
2024-02-25 14:45:10 +0630

    initialize lesson2

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,54 @@
 "use strict";
-let myName = "Ye";
-let meaningOfLife;
-let isLoading = false;
-let album;
-myName = "Johnson";
-meaningOfLife = 5;
-isLoading = true;
-album = 455;
-const sum = (a, b) => {
-  return a + b;
+// Arrays
+let fruitArr = ["Apple", "Orange", "Pineapple"];
+let seller = ["Paul", "Robbery", 678];
+let mixed = ["Chris", 432, false];
+fruitArr[0] = "Onion";
+fruitArr.push("Grape");
+seller[0] = 423;
+seller.unshift("Chains");
+let test = [];
+let bands = [];
+bands.push("Brown");
+// Tuple
+let myTuple = [532, "Kate", true];
+let records = [423, "Berry", false];
+records = myTuple;
+myTuple[1] = "Rob";
+// Objects
+let myObject;
+myObject = [];
+console.log(typeof myObject);
+myObject = bands;
+myObject = {};
+let instanceObj = {
+    propA: "Chase",
+    propB: 23,
 };
-let postId;
-let isActive;
-let exp = /\w+/g;
+instanceObj.propA = "Kelvin";
+let eu = {
+    name: "Flusk",
+    isActive: true,
+    album: [432, "Drone"],
+};
+let jp = {
+    name: "Vivo",
+    album: [43, "Groovy", "Honey"],
+};
+eu = jp;
+const guitarHolder = (guitarList) => {
+    if (guitarList.name) {
+        return `Welcome${guitarList.name.toUpperCase()}`;
+    }
+    return "Superfine";
+};
+console.log(guitarHolder(jp));
+// enums
+var grades;
+(function (grades) {
+    grades[grades["A"] = 98] = "A";
+    grades[grades["B"] = 80] = "B";
+    grades[grades["C"] = 70] = "C";
+    grades[grades["D"] = 60] = "D";
+})(grades || (grades = {}));
+console.log(grades.A);
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,76 @@
-let myName: string = "Ye";
-let meaningOfLife: number;
-let isLoading: boolean = false;
-let album: any;
+// Arrays
+let fruitArr = ["Apple", "Orange", "Pineapple"];
+let seller = ["Paul", "Robbery", 678];
+let mixed = ["Chris", 432, false];
 
-myName = "Johnson";
-meaningOfLife = 5;
-isLoading = true;
-album = 455;
+fruitArr[0] = "Onion";
+fruitArr.push("Grape");
 
-const sum = (a: number, b: string) => {
-  return a + b;
+seller[0] = 423;
+seller.unshift("Chains");
+
+let test = [];
+let bands: string[] = [];
+bands.push("Brown");
+
+// Tuple
+let myTuple: [number, string, boolean] = [532, "Kate", true];
+
+let records = [423, "Berry", false];
+
+records = myTuple;
+
+myTuple[1] = "Rob";
+
+// Objects
+let myObject: object;
+myObject = [];
+console.log(typeof myObject);
+myObject = bands;
+myObject = {};
+
+let instanceObj = {
+  propA: "Chase",
+  propB: 23,
 };
 
-let postId: string | number;
-let isActive: number | boolean;
+instanceObj.propA = "Kelvin";
 
-let exp: RegExp = /\w+/g;
+// type GuitarList = {
+interface GuitarList {
+  name?: string;
+  isActive?: boolean;
+  album: (string | number)[];
+}
+
+let eu: GuitarList = {
+  name: "Flusk",
+  isActive: true,
+  album: [432, "Drone"],
+};
+
+let jp: GuitarList = {
+  name: "Vivo",
+  album: [43, "Groovy", "Honey"],
+};
+
+eu = jp;
+
+const guitarHolder = (guitarList: GuitarList) => {
+  if (guitarList.name) {
+    return `Welcome${guitarList.name.toUpperCase()}`;
+  }
+  return "Superfine";
+};
+
+console.log(guitarHolder(jp));
+
+// enums
+enum grades {
+  A = 98,
+  B = 80,
+  C = 70,
+  D = 60,
+}
+
+console.log(grades.A);
