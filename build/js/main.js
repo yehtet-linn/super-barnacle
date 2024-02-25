c3cef0c626edee0a6639bd173d094df0511b05ce
Ye Htet Lin <yehtet.linyn@gmail.com>
2024-02-25 23:24:33 +0630

    initialize lesson3

--- build/js/main.js
+++ build/js/main.js
@@ -1,24 +1,75 @@
 "use strict";
-// convert it into more specific to less specific
-let a = "welcome";
-let b = a; // less specific
-let c = a; //more specific
-let d = "guy";
-let e = "guy";
-const addOrConcat = (a, b, c) => {
-    if (c === "add")
-        return a + b;
-    return "" + a + b;
-};
-let myVal = addOrConcat(6, 1, "concat");
-// return as string
-let newVal = addOrConcat(9, 2, "add");
-// 60 as string
-60;
-// dom
-const img = document.querySelector("img"); // express non null dom
-const myImg = document.querySelector("#img");
-const nextImg = document.querySelector("#img");
-img.src;
-myImg.src;
-nextImg.src;
+// 1st Phase
+class Developer {
+    constructor(name, music, age, lang = "TypeScript") {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Welcome. Btw, I'm ${this.age}`;
+    }
+}
+const developer = new Developer("Chase", "Rock", 24);
+console.log(developer.getAge());
+class WebDev extends Developer {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I know ${this.lang}`;
+    }
+}
+const webDev = new WebDev("Super Pc", "David", "Spotify", 24);
+console.log(webDev.getLang());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}.`;
+    }
+}
+const artist = new Guitarist("Gross", "guitar");
+console.log(artist.play("plays"));
+// 3rd Phase
+class Team {
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Team.count;
+    }
+}
+Team.count = 0;
+const memberA = new Team("Janes");
+const memberB = new Team("Jonson");
+console.log(memberA.id);
+console.log(memberB.id);
+console.log("no. of instances: " + Team.count);
+// 4th Phase
+class Project {
+    constructor() {
+        this.sources = [];
+    }
+    get data() {
+        return this.sources;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every((val) => typeof val === "string")) {
+            this.sources = value;
+            return;
+        }
+        else {
+            throw new Error("Param is not in an array of strings");
+        }
+    }
+}
+const projA = new Project();
+projA.data = ["Kyaw Kyaw", "Aung Aung"];
+console.log(projA.data);
+projA.data = [...projA.data, "Min Min"];
+console.log(projA.data);
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,107 @@
-type One = string;
-type Two = string | number;
-type Three = "welcome";
+// 1st Phase
+class Developer {
+  secondLang!: string;
 
-// convert it into more specific to less specific
-let a: One = "welcome";
-let b = a as Two; // less specific
-let c = a as Three; //more specific
+  constructor(
+    public readonly name: string,
+    public music: string,
+    private age: number,
+    protected lang: string = "TypeScript"
+  ) {}
 
-let d = <One>"guy";
-let e = <string | number>"guy";
+  public getAge() {
+    return `Welcome. Btw, I'm ${this.age}`;
+  }
+}
 
-const addOrConcat = (
-  a: number,
-  b: number,
-  c: "add" | "concat"
-): number | string => {
-  if (c === "add") return a + b;
-  return "" + a + b;
-};
+const developer = new Developer("Chase", "Rock", 24);
+console.log(developer.getAge());
 
-let myVal: string = addOrConcat(6, 1, "concat") as string;
-// return as string
-let newVal: number = addOrConcat(9, 2, "add") as number;
+class WebDev extends Developer {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age);
+    this.computer = computer;
+  }
 
-// 60 as string
-60 as unknown as string;
+  public getLang() {
+    return `I know ${this.lang}`;
+  }
+}
 
-// dom
-const img = document.querySelector("img")!; // express non null dom
-const myImg = document.querySelector("#img") as HTMLImageElement;
-const nextImg = <HTMLImageElement>document.querySelector("#img");
+const webDev = new WebDev("Super Pc", "David", "Spotify", 24);
+console.log(webDev.getLang());
 
-img.src;
-myImg.src;
-nextImg.src;
+// 2nd Phase
+interface Muscian {
+  name: string;
+  instrument: string;
+  play(action: string): string;
+}
+
+class Guitarist implements Muscian {
+  name: string;
+  instrument: string;
+
+  constructor(name: string, instrument: string) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+
+  play(action: string): string {
+    return `${this.name} ${action} the ${this.instrument}.`;
+  }
+}
+
+const artist = new Guitarist("Gross", "guitar");
+console.log(artist.play("plays"));
+
+// 3rd Phase
+class Team {
+  static count: number = 0;
+
+  public id: number;
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Team.count;
+  }
+}
+
+const memberA = new Team("Janes");
+const memberB = new Team("Jonson");
+
+console.log(memberA.id);
+console.log(memberB.id);
+console.log("no. of instances: " + Team.count);
+
+// 4th Phase
+class Project {
+  private sources: string[];
+
+  constructor() {
+    this.sources = [];
+  }
+
+  public get data(): string[] {
+    return this.sources;
+  }
+
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((val) => typeof val === "string")) {
+      this.sources = value;
+      return;
+    } else {
+      throw new Error("Param is not in an array of strings");
+    }
+  }
+}
+
+const projA = new Project();
+projA.data = ["Kyaw Kyaw", "Aung Aung"];
+console.log(projA.data);
+projA.data = [...projA.data, "Min Min"];
+console.log(projA.data);
